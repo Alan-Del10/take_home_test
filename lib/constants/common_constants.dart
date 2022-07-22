cf8286eb7ac5a68038d964d889ff7e23d2a63154
adelgado <adelgado@krasamo.com>
2022-07-21 20:44:32 -0500

    Removed unnecessary class

--- lib/constants/common_constants.dart
+++
@@ -1,6 +0,0 @@
-class CommonConstants {
-  static String fullScreen = 'fullScreen';
-  static String listWidget = 'listWidget';
-  static String searchBarWidget = 'searchBarWidget';
-  static String recentSearchWidget = 'recentSearchWidget';
-}
